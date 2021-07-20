b9118703dacc7714af72b6f6ee91b6743a06a299
Grzegorz Balcewicz <gbalcewicz@kaliop.com>
2021-07-20 09:17:47 +0200

    Added jest

---
+++ tests/Registry.spec.ts
@@ -0,0 +1,30 @@
+import createRegistry, { Registry } from "../src/Registry";
+
+type Components = {
+  Tag: {
+    name: string
+  },
+  Name: {
+    value: string
+  },
+  Position: {
+    x: number,
+    y: number,
+    z: number
+  }
+};
+
+describe('Registry', () => {
+  let registry: Registry<Components>;
+
+  beforeEach(() => {
+    registry = createRegistry<Components>();
+  })
+  
+  test('creates entities with sequential ids', () => {
+    const entity1 = registry.createEntity();
+    const entity2 = registry.createEntity();
+    expect(entity1.id).toBe(1)
+    expect(entity2.id).toBe(2)
+  })
+})
